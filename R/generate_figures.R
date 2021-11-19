7bc41062fe67da44b911f3c79af85f2bdad34b62
Isaac Thompson <93336839+ifthompson@users.noreply.github.com>
2021-11-19 14:07:11 -0500

    generate_figures options for sector, gcm

--- R/generate_figures.R
+++ R/generate_figures.R
@@ -15,16 +15,16 @@
 
 
 generate_figures <- function(folder=NULL,
-                             temporal_scale = "annual") {
-  NULL -> year -> value -> GCM
+                             sectors="total",
+                             gcm="all",
+                             temporal_scale="annual") {
+  NULL -> Year -> Value -> GCM
 
   # Initialize
   print("Starting generate_figures ...")
 
 
-
   # Check if folder exists
-
   # Check if required files exist (sectors total, irr etc.)
 
   # Annual plots
@@ -38,10 +38,10 @@ generate_figures <- function(folder=NULL,
 
     # Example script
 
-    datax <- build_df(folder)
+    datax <- get_annual_data(folder, sectors=sectors, gcm=gcm)
 
     p1 <- ggplot2::ggplot(data = datax,
-                          ggplot2::aes(x = year, y = value, group = GCM)) +
+                          ggplot2::aes(x = Year, y = Value, group = GCM)) +
       ggplot2::geom_line(ggplot2::aes(color=GCM)) +
       ggplot2::facet_grid(RCP~SSP,scales="fixed") +
       ggplot2::ggtitle("Global Annual Water Withdrawal by SSP-RCP-GCM") +
@@ -70,37 +70,87 @@ generate_figures <- function(folder=NULL,
   } # Close generate_figures function
 
 
-
-build_df <- function(folder=NULL, temporal_scale = "annual") {
-  NULL -> SSP -> RCP -> GCM -> name -> temp -> n -> year
-  combos <- tidyr::crossing(SSP = c("SSP 1", "SSP 2", "SSP 3", "SSP 4", "SSP 5"),
-                            RCP = c("RCP 2.6", "RCP 4.5", "RCP 6.0", "RCP 8.5"),
-                            GCM = c("gfdl", "hadgem", "ipsl", "miroc", "noresm")
-  )
-
+# all valid ssp-rcp-gcm-sector (including total, nonag sectors)
+all_combos <- function() {
+  combos <- tidyr::crossing(SSP = c(ssp1 = "SSP 1",
+                                    ssp2 = "SSP 2",
+                                    ssp3 = "SSP 3",
+                                    ssp4 = "SSP 4",
+                                    ssp5 = "SSP 5"),
+                            RCP = c(rcp26 = "RCP 2.6",
+                                    rcp45 = "RCP 4.5",
+                                    rcp60 = "RCP 6.0",
+                                    rcp85 = "RCP 8.5"),
+                            GCM = c("gfdl",
+                                    "hadgem",
+                                    "ipsl",
+                                    "miroc",
+                                    "noresm"),
+                            Sector = c(dom = "Domestic",
+                                       elec= "Electricity",
+                                       irr = "Irrigation",
+                                       liv = "Livestock",
+                                       mfg = "Manufacturing",
+                                       min = "Mining",
+                                       nonag = "Non-Agriculture",
+                                       total = "Total")
+                            )
   combos <- dplyr::filter(combos, !(SSP=="SSP 3" & RCP=="RCP 2.6") & # if rcp26, exclude ssp3
                             !(SSP!="SSP 5" & RCP=="RCP 8.5") & # if rcp85, must use ssp5
                             !(RCP=="RCP 8.5" & GCM!="gfdl") # if rcp85, must use gfdl
-  )
+                          )
+  return(combos)
+}
 
-  annual <- tidyr::crossing(combos, year=seq(2010,2100,by=5)) # each year for each combo
+get_paths <- function(folder=NULL, combos) {
+  files <- dplyr::transmute(combos, paste0(
+    folder, "/", names(SSP), "_",names(RCP), "_", GCM,
+    "/wd", names(Sector), "_km3peryr.csv"))[[1]]
+  return(files)
+}
 
-  folders <- dplyr::transmute(combos, name=tolower(
-    (gsub(" |\\.", "",paste(SSP,RCP,GCM,sep="_"))))) # eg ssp1_rcp26_gfdl
 
-  paths <- dplyr::transmute(folders, path=paste0(
-    folder, "\\", name, "\\wdtotal_km3peryr.csv"))[[1]]
+# get annual data for a single sector, all sectors, or totals
+get_annual_data <- function(folder=NULL,
+                            sectors="total",
+                            gcm="all") {
+  NULL -> SSP -> RCP -> GCM -> Year
 
-  #paths <- list.files(path=folder, full.names = TRUE, recursive = TRUE, pattern="*.csv")
-  DT <- do.call(rbind, lapply(paths, function(x){colSums(data.table::fread(x, drop=1:5))}))
-  dplyr::as_tibble(DT)%>%
-    dplyr::mutate(n=1:dplyr::n())%>%
-    tidyr::gather(key="year", value="value", -n)%>%
-    dplyr::arrange(n)%>%
-    dplyr::select(-n,-year)%>%
-    dplyr::bind_cols(annual) ->
-    df
-  #df <- cbind(annual, stack(data.frame(t(DT)))[1])
+  combos <- all_combos() # 71 valid ssp-rcp-gcm * 6+2 sectors/totals
 
+  # filter to only relevant sectors or gcms
+  if (sectors=="all") {
+    print("Using all sectors (except 'total' and 'nonag')")
+    combos <- dplyr::filter(combos, (names(Sector)!="total") &
+                               (names(Sector)!="nonag"))
+  }
+  if (is.element(sectors, c("dom","elec","irr","liv","mfg","min","nonag","total"))) {
+    print(paste0("Using sector: ", sectors))
+    combos <- dplyr::filter(combos, (names(Sector)==sectors))
+  }
+  if (is.element(gcm, c("gfdl", "hadgem", "ipsl", "miroc", "noresm"))) {
+    print(paste0("Using GCM: ", gcm))
+    combos <- dplyr::filter(combos, (GCM==gcm))
+  }
+
+  files <- get_paths(folder, combos) # list of file paths
+
+  print(paste0("Loading data from ", length(files), " files ..."))
+
+  values <- data.table::rbindlist(lapply(files, function(x){ # stacks long
+    data.frame(Value=colSums(data.table::fread(x, drop=1:5)))})) # sums of year columns
+
+  print("Data loaded")
+
+  annual <- tidyr::crossing(combos, Year=seq(2010,2100,by=5)) # each year for each combo
+  df <- cbind(annual, values) # SSP,RCP,GCM,
+
+  # compute mean of 5 GCMs if requested (instead of, not in addition to)
+  if (gcm=="avg") {
+    print("Computing mean of 5 GCMs ...")
+    df <- dplyr::group_by(df, SSP, RCP, Sector, Year)
+    df <- dplyr::summarise(df, Value=mean(Value))
+    df <- tibble::add_column(df, GCM=rep("avg", nrow(df)), .after="RCP")
+  }
   return(df)
 }
--- inst/extras/devTests.R
+++ inst/extras/devTests.R
@@ -5,31 +5,10 @@
 # Install and Restart
 
 
-folder_i="C:\\Users\\thom927\\Documents\\Data\\tethysDemeterOutputs"
-temporal_scale_i = "annual"
+folder_i="C:/Users/thom927/Documents/Data/tethysDemeterOutputs"
 
 out <- generate_figures(folder = folder_i,
-                 temporal_scale = temporal_scale_i)
+                        sectors="total",
+                        gcm="all",
+                        temporal_scale = "annual")
 
-
-combos <- tidyr::crossing(SSP = c("SSP 1", "SSP 2", "SSP 3", "SSP 4", "SSP 5"),
-                          RCP = c("RCP 2.6", "RCP 4.5", "RCP 6.0", "RCP 8.5"),
-                          GCM = c("gfdl", "hadgem", "ipsl", "miroc", "noresm")
-)
-
-combos <- dplyr::filter(combos, !(SSP=="SSP 3" & RCP=="RCP 2.6") & # if rcp26, exclude ssp3
-                      !(SSP!="SSP 5" & RCP=="RCP 8.5") & # if rcp85, must use ssp5
-                      !(RCP=="RCP 8.5" & GCM!="gfdl") # if rcp85, must use gfdl
-)
-
-annual <- tidyr::crossing(combos, year=seq(2010,2100,by=5))
-
-folders <- dplyr::transmute(combos, name=tolower(
-  (gsub(" |\\.", "",paste(SSP,RCP,GCM,sep="_"))))) # eg ssp1_rcp26_gfdl
-
-paths <- dplyr::transmute(folders, path=paste0(
-  folder_i, "\\", name, "\\wdtotal_km3peryr.csv"))[[1]]
-
-#temp <- list.files(path=folder_i, full.names = TRUE, recursive = TRUE, pattern="*.csv")
-DT <- do.call(rbind, lapply(paths, function(x){t(colSums(data.table::fread(x, drop=1:5)))}))
-df <- cbind(annual, stack(data.frame(t(DT)))[1])
