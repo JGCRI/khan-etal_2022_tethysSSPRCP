32debfd4702e31b3fb6b522d722faece9e11247c
Isaac Thompson <93336839+ifthompson@users.noreply.github.com>
2021-12-07 19:56:34 -0500

    Validation figures

--- inst/extras/devTests.R
+++ inst/extras/devTests.R
@@ -4,9 +4,11 @@
 # Go to more>document
 # Install and Restart
 
+folder = "C:/Users/thom927/Documents/Data/tethysDemeterOutputs"
+GCAM_withdrawals_csv = "C:/Users/thom927/Documents/Data/GrahamGCAM/water_withdrawals_by_mapping_source.csv"
 
 out <- generate_figures(annual_rds = "annual_data.rds",
                         monthly_rds = "monthly_data.rds",
                         folder = "C:/Users/thom927/Documents/Data/tethysDemeterOutputs",
-                        temporal_scale = "monthly")
+                        temporal_scale = "annual")
 
